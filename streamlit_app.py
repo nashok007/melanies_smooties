715716e6c20b3ddb7957bb142c09338c13117bfe
nashok007 <47435016+nashok007@users.noreply.github.com>
2024-12-14 15:18:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ ingredients_list =st.multiselect(
 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
     
     ingredients_string=''
     
@@ -39,7 +39,7 @@ if ingredients_list:
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
