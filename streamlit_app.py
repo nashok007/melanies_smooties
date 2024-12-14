b9b0c3f182d76ad2e01070591eaf456d0869bcaa
nashok007 <47435016+nashok007@users.noreply.github.com>
2024-12-14 15:16:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,6 @@ ingredients_list =st.multiselect(
     ,my_dataframe
     ,max_selections=5
 )
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 if ingredients_list:
     st.write(ingredients_list)
@@ -37,6 +34,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
     st.write(ingredients_string)
     
